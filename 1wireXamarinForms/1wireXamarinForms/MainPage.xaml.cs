cb8343101566c516d836f4a7d74a879109ebfb74
Bogdan Lototsky <Bogdan.Lototsky@fillinco.com>
2021-02-02 18:13:18 +0200

    Add project files.

---
+++ 1wireXamarinForms/1wireXamarinForms/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using _1wireXamarinForms.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.IO.Ports;
+namespace _1wireXamarinForms
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<MyDevice> Devices { get; set; }
+
+        public SerialPort port;
+        public MainPage()
+        {
+            InitializeComponent();
+            Devices = new List<MyDevice>
+            {
+                new MyDevice {DeviceName="ds1922l",Description="this is device ds1922l" },
+                new MyDevice {DeviceName="ds1923",Description="this is device ds1923"},
+                new MyDevice {DeviceName="ds1992",Description="this is device ds1992" }
+            };
+            this.BindingContext = this;
+
+        }
+
+        public async void OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            MyDevice selectedDevice = e.Item as MyDevice;
+            if (selectedDevice != null)
+                await DisplayAlert("Выбранная модель", $"{selectedDevice.DeviceName} - {selectedDevice.Description}", "OK");
+        }
+
+        public async void OnRefreshDevicesButtonClicked(object sender, EventArgs args)
+        {
+            checkport();
+
+        }
+
+        public async void  checkport()
+        {
+            port = new SerialPort(port.PortName=SetPortName(port.PortName), 9600, Parity.None, 0, StopBits.One);
+
+            port.Open();
+
+
+
+        }
+
+        public static string SetPortName(string defaultPortName)
+        {
+            string portName;
+
+            Console.WriteLine("Available Ports:");
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                Console.WriteLine("   {0}", s);
+            }
+
+            Console.Write("Enter COM port value (Default: {0}): ", defaultPortName);
+            portName = Console.ReadLine();
+
+            if (portName == "" || !(portName.ToLower()).StartsWith("com"))
+            {
+                portName = defaultPortName;
+            }
+            return portName;
+        }
+
+
+    }
+}
---
+++ 1wireXamarinForms/1wireXamarinForms/Models/MyDevice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1wireXamarinForms.Models
+{
+    public class MyDevice
+    {
+        public string DeviceName { get; set; }
+
+
+        public string Description { get; set; }
+
+
+
+
+
+    }
+}
